2d391211a379f09320188b2feff95cfe85a1b6a7
SongJi-Won <s26788761@naver.com>
2019-08-18 22:23:15 +0900

    #2.0 creating a react component

---
+++ 2 JSX & Props/movie_app_2019/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Jiwon from './Jiwon';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>this is react example</h1>
+      <Jiwon />
+
+    </div>
+  );
+}
+
+export default App;
---
+++ 2 JSX & Props/movie_app_2019/src/Jiwon.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Jiwon() {
+    return <h3>This is Jiwon Component</h3>;
+}
+
+export default Jiwon;
