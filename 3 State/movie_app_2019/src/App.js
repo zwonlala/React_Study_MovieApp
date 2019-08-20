3bdbdf76292cf42d8a9a4cecc91e0c24b7529728
SongJi-Won <s26788761@naver.com>
2019-08-20 15:32:22 +0900

    #3.1 how to modify State - "setStage()" & using "current"

--- 3 State/movie_app_2019/src/App.js
+++ 3 State/movie_app_2019/src/App.js
@@ -2,23 +2,40 @@ import React from 'react';
 
 class App extends React.Component {
   state = {
-    count: 0
+    count: 0,
+    discount: 0
   };
 
   add = () => {
-    console.log("add");
+    this.setState(current => ({ count: current.count+1 }));
+    this.setState(current => ({ x: current.count+1 }));
+    console.log(this.state);
   }
 
   minus = () => {
-    console.log("minus");
+    this.setState(current => ({ count: current.count-1 }));
+  }
+
+  add2 = () => {
+    this.setState({ discount: this.state.discount+1 });
+  }
+
+  minus2 = () => {
+    this.setState({ discount: this.state.discount-1 });
   }
   render() {
     return (
       <div>
         <h1>The number is {this.state.count}</h1>
+        <h1>The discount number is {this.state.discount}</h1>
+        <h1>X is {this.state.x}</h1>
+
         <button onClick={this.add}>Add</button>
         <button onClick={this.minus}>Minus</button>
 
+        
+        <button onClick={this.add2}>Add2</button>   
+        <button onClick={this.minus2}>Minus2</button>
       </div>
 
     );
