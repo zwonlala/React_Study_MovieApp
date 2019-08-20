f1c8c33e2f327df0e1d076f94a7d631003962a1c
SongJi-Won <s26788761@naver.com>
2019-08-20 14:43:05 +0900

    #3.0 using State by Class Component

---
+++ 3 State/movie_app_2019/src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+class App extends React.Component {
+  state = {
+    count: 0
+  };
+
+  add = () => {
+    console.log("add");
+  }
+
+  minus = () => {
+    console.log("minus");
+  }
+  render() {
+    return (
+      <div>
+        <h1>The number is {this.state.count}</h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
+
+      </div>
+
+    );
+  }
+}
+
+export default App;
